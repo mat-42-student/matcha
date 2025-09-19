f28ffe9ce6060a1d663922ee5ff0aa68c19e7faa
matgit68 <mamat@mamat-OptiPlex-7010>
2025-09-19 20:09:00 +0200

    tofix : text of button won't display accurate info

--- matcha/src/app/api/match/[userId]/like/route.ts
+++ matcha/src/app/api/match/[userId]/like/route.ts
@@ -1,15 +1,13 @@
-// matcha/src/app/api/match/[userId]/like/route.ts
-import { NextResponse, NextRequest } from "next/server";
-import { getUserByIdFromSession } from "@/lib/db/session";
 import { pool } from "@/lib/db/db-utils";
 import { cookies } from "next/headers";
-
-
+import { NextResponse } from "next/server";
+import { getUserByIdFromSession } from "@/lib/db/session";
 
 export async function POST(
   req: Request,
   context: { params: Promise<{ userId: string }> }
 ) {
+  const client = await pool.connect();
   try {
     const { userId } = await context.params;
     const cookieStore = await cookies();
@@ -28,16 +26,37 @@ export async function POST(
       return NextResponse.json({ error: "Narcissistic" }, { status: 400 });
     }
 
-    const query = `
-      INSERT INTO matches (user1_id, user2_id, status)
-      VALUES ($1, $2, 'like')
-      ON CONFLICT (user1_id, user2_id) DO NOTHING
-    `;
-    await pool.query(query, [me.id, userId]);
+    await client.query("BEGIN");
+
+    const existing = await client.query(
+      `SELECT * FROM matches 
+       WHERE user1_id = $1 AND user2_id = $2`,
+      [userId, me.id]
+    );
+
+    if (existing.rows.length > 0 && existing.rows[0].status === "like") {
+      await client.query(
+        `UPDATE matches SET status = 'match' 
+         WHERE user1_id = $1 AND user2_id = $2`,
+        [userId, me.id]
+      );
+    } else {
+      await client.query(
+        `INSERT INTO matches (user1_id, user2_id, status)
+         VALUES ($1, $2, 'like')
+         ON CONFLICT (user1_id, user2_id) DO NOTHING`,
+        [me.id, userId]
+      );
+    }
+
+    await client.query("COMMIT");
 
     return NextResponse.json({ success: true });
   } catch (err) {
+    await client.query("ROLLBACK");
     console.error(err);
     return NextResponse.json({ error: "Erreur serveur" }, { status: 500 });
+  } finally {
+    client.release(); // Libérer la connexion
   }
 }
--- matcha/src/components/card-user/LikeButton.tsx
+++ matcha/src/components/card-user/LikeButton.tsx
@@ -1,27 +1,62 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PublicUser } from "@/types";
 
 export default function LikeButton({ user }: { user: PublicUser }) {
   const [liked, setLiked] = useState(false);
+  const [status, setStatus] = useState("");
 
-  function handleLike(e: React.MouseEvent) {
-    e.stopPropagation();
-    console.log("fetch vers : ", user.id)
-    setLiked(!liked);
-    fetch(`/api/match/${user.id}/like`, { method: "POST" })
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
+  async function fetchLikeStatus() {
+    try {
+      const res = await fetch(`/api/match/${user.id}/status`);
+      const data = await res.json();
+
+      if (data.status === "like") {
+        setLiked(true);
+        setStatus("");
+      } else if (data.status === "match") {
+        setLiked(true);
+        setStatus("match")
+      } else if (data.status === "isLiked") {
+        setLiked(false);
+        setStatus("isLiked")
+      } else {
+        setLiked(false)
+        setStatus("")
+      }
+    } catch (error) {
+      console.error("Error fetching like status:", error);
+    }
   }
 
+  async function handleLike(e: React.MouseEvent) {
+    e.stopPropagation();
+    let url = '';
+    if (!liked)
+      url = `/api/match/${user.id}/like`;
+    else
+      url = `/api/match/${user.id}/unlike`;
+    try {
+      const res = await fetch(url, { method: "POST" });
+      const data = await res.json();
+      if (data.success) setLiked(!liked);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => { fetchLikeStatus() }, []);
+
   return (
-    <button
-        className="bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 transition"
-        onClick={(e) => { handleLike(e); }}
-    >
-      {liked ? "Unlike" : "Like"}
-    </button>
+    <>
+      <button
+          className="bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 transition"
+          onClick={(e) => { handleLike(e); }}
+      >
+        {status === "match" ? "Unmatch" : liked ? "Unlike" : "Like"}
+      </button>
+      {status === "isLiked" && <span className="text-pink-800">(❤️ Likes you)</span>}
+    </>
   );
 }
